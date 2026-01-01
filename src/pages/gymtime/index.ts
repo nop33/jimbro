b0f0587b7441d18a17782b042bb75d5e5d4061b2
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-01-01 17:21:19 +0800

    Remove vibration pattern

--- src/pages/gymtime/index.ts
+++ src/pages/gymtime/index.ts
@@ -186,22 +186,19 @@ const sendBreakFinishedNotification = () => {
   const notificationBody = 'Time to start your next set!'
   const notificationIcon =
     'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💪</text></svg>'
-  const notificationVibrate = [200, 100, 200]
 
   if ('Notification' in window) {
     if (Notification.permission === 'granted') {
       new Notification(notificationTitle, {
         body: notificationBody,
-        icon: notificationIcon,
-        vibrate: notificationVibrate
+        icon: notificationIcon
       })
     } else if (Notification.permission !== 'denied') {
       Notification.requestPermission().then((permission) => {
         if (permission === 'granted') {
           new Notification(notificationTitle, {
             body: notificationBody,
-            icon: notificationIcon,
-            vibrate: notificationVibrate
+            icon: notificationIcon
           })
         }
       })
