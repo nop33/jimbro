e238594d7304ad2020305a016d2a429bebc322de
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-01-03 18:41:25 +0800

    Add install button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import './style.css'
+
+const installBtn = document.getElementById('install-btn') as HTMLButtonElement
+let deferredPrompt: Event | null = null
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault()
+  deferredPrompt = e
+})
+
+installBtn.addEventListener('click', () => {
+  if (deferredPrompt && 'prompt' in deferredPrompt && typeof deferredPrompt.prompt === 'function') {
+    deferredPrompt.prompt()
+  }
+})
+
+window.addEventListener('appinstalled', () => installBtn.remove())
