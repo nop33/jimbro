d61a30c8f1688098f1bd7f747672e392e517070b
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-01-06 11:07:07 +0800

    Add haptic to workout card

--- src/features/hapticFeedback/index.ts
+++ src/features/hapticFeedback/index.ts
@@ -1,7 +1,7 @@
 document.addEventListener('click', (e) => {
   const target = e.target as HTMLElement
 
-  if (target.closest('button') || target.closest('a') || target.closest('summary')) {
-    navigator.vibrate(20)
+  if (target.closest('button') || target.closest('a') || target.closest('summary') || target.closest('.light-haptic')) {
+    navigator.vibrate(5)
   }
 })
