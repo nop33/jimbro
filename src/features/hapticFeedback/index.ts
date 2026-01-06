52932ce83734590502ae5d05e2dc473942ec0809
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-01-06 11:09:01 +0800

    Reduce vibration

--- src/features/hapticFeedback/index.ts
+++ src/features/hapticFeedback/index.ts
@@ -2,6 +2,6 @@ document.addEventListener('click', (e) => {
   const target = e.target as HTMLElement
 
   if (target.closest('button') || target.closest('a') || target.closest('summary') || target.closest('.light-haptic')) {
-    navigator.vibrate(5)
+    navigator.vibrate(2)
   }
 })
