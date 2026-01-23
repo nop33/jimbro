b9503c0ebac41c770321f8291de64447817285e8
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-01-23 11:07:17 +0100

    Request browser persistent storage

--- src/db/storage.ts
+++ src/db/storage.ts
@@ -2,6 +2,16 @@ import { DB_NAME } from './constants'
 import { getLatestDbVersion, getMigrationForVersion } from './migrations'
 import { promisifyRequest } from './utils'
 
+if (navigator.storage && navigator.storage.persist) {
+  navigator.storage.persist().then((persistent) => {
+    if (persistent) {
+      console.log('Storage will not be cleared except by explicit user action')
+    } else {
+      console.log('Storage may be cleared by the UA under storage pressure.')
+    }
+  })
+}
+
 const openDatabase = async (): Promise<IDBDatabase> => {
   return new Promise((resolve, reject) => {
     const latestVersion = getLatestDbVersion()
