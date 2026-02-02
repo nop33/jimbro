e346cb7621735a9818031fba22cc3f4feecf33cf
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-02-02 23:07:15 +0100

    Add HTML partials to keep HTML code DRY

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
+import injectHTML from 'vite-plugin-html-inject'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [injectHTML(), tailwindcss()],
   build: {
     rolldownOptions: {
       input: {
