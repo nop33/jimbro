e795dec9551f9d7f5128701712489a6e09e54d18
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-03-15 16:39:52 +0100

    Adjust tests timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,14 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 60_000,
+  expect: {
+    timeout: 10_000,
+  },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
