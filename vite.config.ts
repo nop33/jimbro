e89e120f7b2b8dd91ba8ec069c04381b5c3098d1
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-03-17 22:07:01 +0100

    Add missing entry points

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite-plus'
 
 export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        home: 'index.html',
+        exercises: 'exercises/index.html',
+        programs: 'programs/index.html',
+        settings: 'settings/index.html',
+        workouts: 'workouts/index.html',
+        gymtime: 'gymtime/index.html'
+      }
+    }
+  },
   fmt: {
     singleQuote: true,
     semi: false,
